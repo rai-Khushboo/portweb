f91f0dcb960731b91364d4adade992343023b414
rai-Khushboo <khushboo83687@gmail.com>
2024-10-26 11:58:17 +0530

    projects done

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,49 +8,70 @@ import { FreeMode, Pagination, Navigation } from 'swiper/modules';
 import { FaChevronLeft, FaChevronRight, FaExternalLinkAlt } from 'react-icons/fa';
 //importing images
 import beach from '../assets/img/beach.jpg';
+import curr from '../assets/img/curr.jpg';
+import amazon from '../assets/img/amazon.jpg';
+import Feedback from '../assets/img/Feedback.jpg';
+import Citizen from '../assets/img/Citizen.jpg';
+import myntra from '../assets/img/myntra.jpg';
+import reactPwd from '../assets/img/reactPwd.jpg';
+import mini from '../assets/img/mini.jpg';
 
 const projects = [
   {
     title: "Beach Website",
-    description: "Resort Website created using React js and tailwind css.",
+    description: "A resort website built with React.js and Tailwind CSS, showcasing beautiful beach destinations.",
     thumbnail: beach,
     githubLink: "https://github.com/rai-Khushboo/React/tree/main/Website-beach",
     liveLink: "https://react-beryl-kappa.vercel.app/"
   },
   {
-    title: "Project 2",
-    description: "Description of project 2.",
-    thumbnail: beach,
-    githubLink: "https://github.com/rai-Khushboo",
-    liveLink: "https://rai-Khushboo.com"
+    title: "Currency Converter",
+    description: "A user-friendly currency converter application using React, providing real-time exchange rates.",
+    thumbnail: curr,
+    githubLink: "https://github.com/rai-Khushboo/React/tree/main/currencyConverter",
+    liveLink: "https://react-h69x.vercel.app/"
   },
   {
-    title: "Project 3",
-    description: "Description of project 3.",
-    thumbnail: beach,
-    githubLink: "https://github.com/rai-Khushboo",
-    liveLink: "https://rai-Khushboo.com"
+    title: "Amazon dot com Clone",
+    description: "A clone of Amazon.com created with HTML, CSS, and JavaScript, replicating key functionalities.",
+    thumbnail: amazon,
+    githubLink: "https://github.com/rai-Khushboo/amazon.com-Clone",
+    liveLink: "https://github.com/rai-Khushboo/amazon.com-Clone"
   },
   {
-    title: "Project 4",
-    description: "Description of project 4.",
-    thumbnail: beach,
-    githubLink: "https://github.com/rai-Khushboo",
-    liveLink: "https://rai-Khushboo.com"
+    title: "FMS JSS Noida",
+    description: "A web application using PHP, SQL, and JavaScript for managing feedback of Faculty in the College",
+    thumbnail: Feedback,
+    githubLink: "https://github.com/rai-Khushboo/FeedbackSystem",
+    liveLink: "https://github.com/rai-Khushboo/FeedbackSystem"
   },
   {
-    title: "Project 5",
-    description: "Description of project 5.",
-    thumbnail: beach,
-    githubLink: "https://github.com/rai-Khushboo",
-    liveLink: "https://rai-Khushboo.com"
+    title: "Nagrik Aur Samvidhan SIH@2024",
+    description: "A collaborative project focused on Indian citizen rights and duties, developed as part of a hackathon.",
+    thumbnail: Citizen,
+    githubLink: "https://github.com/Garimas10u/SIH2024",
+    liveLink: "https://sih-2024-zeta.vercel.app/"
   },
   {
-    title: "Project 6",
-    description: "Description of project 6.",
-    thumbnail: beach,
-    githubLink: "https://github.com/rai-Khushboo",
-    liveLink: "https://rai-Khushboo.com"
+    title: "WeForShe",
+    description: "A project developed for the Myntra Hackerramp@24, addressing the clothing needs and preferences of the Gen-Z audience.",
+    thumbnail: myntra,
+    githubLink: "https://github.com/rai-Khushboo/Myntra-TechSirens",
+    liveLink: "https://github.com/rai-Khushboo/Myntra-TechSirens"
+  },
+  {
+    title: "Random PWD Generator",
+    description: "A React application that generates random passwords for enhanced security.",
+    thumbnail: reactPwd,
+    githubLink: "https://github.com/rai-Khushboo/React-Pwd-Generator/",
+    liveLink: "https://react-pwd-generator-k7u6.vercel.app/"
+  },
+  {
+    title: "Some Mini Project",
+    description: "A collection of mini projects built using HTML, CSS, and JavaScript, showcasing basic web development skills.",
+    thumbnail: mini,
+    githubLink: "https://github.com/rai-Khushboo/WebPulse",
+    liveLink: "https://github.com/rai-Khushboo/WebPulse/"
   },
 ];
 
@@ -72,8 +93,8 @@ const ProjectSlider = () => {
           <SwiperSlide key={project.title} className="flex justify-center p-4">
             <div className="bg-gray-900 shadow-lg rounded-lg p-3 w-80 h-96 flex flex-col">
               <img src={project.thumbnail} alt={project.title} className="w-full h-48 object-fit rounded" />
-              <h3 className="text-lg font-semibold mt-2">{project.title}</h3>
-              <p className="text-gray-200">{project.description}</p>
+              <h3 className="text-lg text-white font-semibold mt-2">{project.title}</h3>
+              <p className="text-blue-300">{project.description}</p>
               
               <div className="mt-10 flex">
                 <a 
